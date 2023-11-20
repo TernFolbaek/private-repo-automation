bcaa08d02ebef1864edce41cf9b869baf8f8c2ef
Tern Folbaek <tfolbaek@gmail.com>
2023-11-20 16:57:26 +0100

    removal of sensitive information

--- script.py
+++ script.py
@@ -2,10 +2,10 @@ import requests
 
 # Your GitHub username and Personal Access Token
 username = 'TernFolbaek'
-token = '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'
+token = 'PAT'
 
 # List of repositories to make private
-repositories = ['mental-arithmetic', 'keymaster-backend', 'videos-platform','decision-trees','binary-multiclass-classification','linked-list', 'angry-chatbot','blog-backend','blog-frontend','welcome-to-open-source','linear-and-logistic-regression','language-identifier','salary-prediction-ml','facial-detection','shopping-cart']
+repositories = ['repo1','repo2','repo3']
 
 # API URL
 api_url = 'https://api.github.com/repos/'
